d4fcaa30ad29211e554ee8bece82463894b4e9d4
henryk1229 <henryk1229@gmail.com>
2019-04-28 13:03:36 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require "Pry"
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    #@artist = artist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    self.artist ? artist.name : nil
+  end
+
+
+
+end
